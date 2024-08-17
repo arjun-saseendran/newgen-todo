13944857434012ace373b9dc3520c64320536b8c
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-17 12:54:55 +0530

    add addTodo function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-
+import {TodoProvider} from './contexts'
+import {useState} from 'react'
 
 
 function App() {
-  
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) =>{
+setTodos((prev) => [{id: Date.now(), ...todo}, ...prev])
+  }
 
   return (
-    <>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+
+    <h1>Test</h1>
       
-    </>
+    </TodoProvider>
   )
 }
 
