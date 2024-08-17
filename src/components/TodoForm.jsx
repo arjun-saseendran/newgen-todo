c37fcfccfa9911e447bbdff7e8caa06bd93a5b42
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-17 18:10:27 +0530

    add TodoForm

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -3,9 +3,28 @@ import {useTodo} from '../contexts/TodoContext'
 
 const TodoForm = () => {
     const [todo, setTodo] = useState('');
-    useTodo()
+    const {addTodo} = useTodo();
+    const add = (e) => {
+        e.preventDefault()
+        if(!todo) return
+        addTodo({todo, completed: false})
+        setTodo('')
+    }
   return (
-    <div>TodoForm</div>
+    <form onSubmit={add} className='flex'>
+    <input type="text" 
+    placeholder='Write Todo...'
+    className='w-full border border-black/10 
+    rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5'
+    value={todo}
+    onChange={(e) => setTodo(e.target.value)}
+/>
+
+<button type='submit' className='rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0'>
+Add
+</button>
+
+    </form>
   )
 }
 
