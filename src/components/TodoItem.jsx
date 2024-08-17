11ebe74d67306dce7e0a4de84bdd9b0515510a57
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-17 18:22:46 +0530

    add TodoItem

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import { useTodo } from '../contexts/TodoContext';
+
+const TodoItem = ({todo}) => {
+    const [isTodoEditable, setIsTodoEditable] = useState(false);
+    const [todoMessage, setTodoMessage] = useState(todo.todo);
+    const {updateTodo, deleteTodo, toggleComplete} = useTodo();
+    const editTodo = () => {
+        updateTodo(todo.id, {...todo, todo: todoMessage})
+        setIsTodoEditable(false)
+    }
+  return (
+    <div>TodoItem</div>
+  )
+}
+
+export default TodoItem
