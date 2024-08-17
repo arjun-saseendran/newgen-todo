5186dff24047eb998c887578c3090d865b48b5e3
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-17 13:02:02 +0530

    add todoUpdate function

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ function App() {
 setTodos((prev) => [{id: Date.now(), ...todo}, ...prev])
   }
 
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === todo.id ? todo : prevTodo)))
+  }
+
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
 
